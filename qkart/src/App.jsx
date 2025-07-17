1bb35d94c330de0d34b6f76d822a87e8148ec853
Husain Ali <husainali7865253@gmail.com>
2025-07-17 15:12:45 +0530

    add poppins

--- qkart/src/App.jsx
+++ qkart/src/App.jsx
@@ -1,10 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
 
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 }
 
